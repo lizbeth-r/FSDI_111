01313b951dfe3924dfd95e49228cdfe571d91b54
Kale e <kalee@Kales-MacBook-Pro.local>
2024-10-10 00:42:52 -0700

    Assignment 1

--- routes.py
+++ routes.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.get("/aboutme")
 def get_home():
     me = {
-        "first_name": "Lizbeth",
-        "last_name": "Ramirez",
-        "hobby": "Visit Coffee shops",
-        "is_online": True
+        "first_name": "LIZBETH",
+        "last_name": "RAMIREZ",
+        "hobby": "VISIT_COFFEE_SHOPS",
+        # "is_online": True
     }
 
     return me
